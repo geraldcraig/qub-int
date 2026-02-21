3c0a6cbfa4fe251c009ceb9515e9fbad5ae2e892
geraldcraig <geraldcraig@me.com>
2026-02-21 20:28:49 +0000

    week 1 added

---
+++ Week-01-Variables-And-Operators/src/main/java/uk/ac/qub/week1/HelloWorld.java
@@ -0,0 +1,26 @@
+/**
+ * Package info goes here
+ */
+package uk.ac.qub.week1;
+
+/**
+ * A class to output simple message
+ * to screen
+ * @author geraldcraig
+ *
+ */
+public class HelloWorld {
+
+    /**
+     * Main method. Start point for the
+     * class. Prints message to screen
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // print to screen
+        System.out.println("Hello World");
+
+    }
+
+}
